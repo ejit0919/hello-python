1ead126834c179b057c429990d23a5567253b2ea
ejit0919 <ejit_0919@yahoo.com>
2018-10-25 23:45:09 +0800

    Editted Activity 2

--- Tanay_e2.py
+++ Tanay_e2.py
@@ -1,12 +1,14 @@
 import sys
 
-if float(sys.argv[1]) >= 220: 
+wind = float(sys.argv[1])
+
+if wind >= 220: 
     print("Super Typhoon")
-elif float(sys.argv[1]) >= 118 and float(sys.argv[1]) <220:
+elif wind >= 118 and wind <220:
     print("Typhoon")
-elif float(sys.argv[1]) >= 89 and float(sys.argv[1]) <118 :
+elif wind >= 89 and wind <118:
     print("Severe Tropical Storm")
-elif float(sys.argv[1]) >= 62 and float(sys.argv[1]) <89:
+elif wind >= 62 and wind <89:
     print("Tropical Storm")
-elif float(sys.argv[1]) <=61 :
+elif wind <=61 :
     print("Tropical Depression")
