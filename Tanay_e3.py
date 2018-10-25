bc4b7924c59284d473fa001721a86c2294955222
ejit0919 <ejit_0919@yahoo.com>
2018-10-26 00:49:07 +0800

    Added Activity 3

---
+++ Tanay_e3.py
@@ -0,0 +1,11 @@
+user_input = input("Enter a comma separated list of numbers: ")
+user_input = user_input.split(",")
+
+index = 0
+sum = 0
+
+for x in user_input:
+    sum += float(user_input[index])**2
+    index += 1
+
+print("Sum of squares:", sum)
